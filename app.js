b0915b75ad0bf8a3c92049e09a305aa1be91f4dc
James Rosa <JROSA10.tv@gmail.com>
2023-06-30 21:09:35 -0400

    final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const cardTypes = [
+const reviews = [
  {
   id: 1,
   name: "King",
   job: "Pusher",
-  img: "https://t4.ftcdn.net/jpg/00/53/65/65/360_F_53656507_6aRlz7GvglpFMAbtf1zSsllWlepJfeTb.jpg",
+  img:
+   "https://t4.ftcdn.net/jpg/00/53/65/65/360_F_53656507_6aRlz7GvglpFMAbtf1zSsllWlepJfeTb.jpg",
   text: "The kinds job is to push the fron line to the back, he can also pull the backline forward but it ends your turn"
  },
  {
@@ -27,3 +28,57 @@ const cardTypes = [
   img: "https://i.pinimg.com/736x/b4/1b/2b/b41b2bb30b0e1aafc50a13e7ae6939f9--joker-card-jester.jpg",
   text: "Able to do evertything but doesnt have a plus three attribute"
  }]
+
+// select items 
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+ const item = reviews[currentItem];
+ img.src = item.img;
+ author.textContent = item.name;
+ job.textContent = item.job;
+ info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+ const item = reviews[person];
+ img.src = item.img;
+ author.textContent = item.name;
+ job.textContent = item.job;
+ info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener('click', function () {
+ currentItem++;
+ if (currentItem > reviews.length - 1) {
+  currentItem = 0;
+ }
+ showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+ currentItem--;
+ if (currentItem < 0) {
+  currentItem = reviews.length - 1;
+ }
+ showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+
+ currentItem = Math.floor(Math.random() * reviews.length);
+ showPerson(currentItem);
+});
